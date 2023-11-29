b1417b61e7641d2287c1bab7079b44d5e9a3fdcc
Yoon <yoonsees@gmail.com>
2023-11-28 23:13:25 -0600

    Add multiplesOf3Or5 and Stop gninnipS My sdroW

---
+++ Stop gninnipS My sdroW.ts
@@ -0,0 +1,6 @@
+/* Instruction: Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.*/
+
+export function spinWords(words: string): string {
+    const splitWords: string[] = words.split(" ")
+    return splitWords.map((word: string) => word.length < 5 ? word : word.split("").reverse().join("")).join(" ")
+}
